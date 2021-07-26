dd0d1d08b7dfde3fda6331fddef417ac508ab780
Tomer Babila <tomerb@academy-skills.com>
2021-07-26 17:06:15 +0300

    Get all restaurants and restaurant by id

--- api/restaurantRouter.js
+++ api/restaurantRouter.js
@@ -1,9 +1,27 @@
 const { Router } = require('express');
+const restaurantModel = require('../models/restaurantModel');
 
 const router = Router();
 
-router.get('/', (req, res) => {
-  res.send('hello restaurant');
+// [GET] - all restaurants
+router.get('/', async (req, res) => {
+  try {
+    const restaurants = await restaurantModel.find({});
+    res.json(restaurants);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+// [GET] - restaurant by id
+router.get('/:id', async (req, res) => {
+  try {
+    const restaurantId = req.params.id;
+    const restaurant = await restaurantModel.findById(restaurantId);
+    res.json(restaurant);
+  } catch (error) {
+    res.status(500).send(error);
+  }
 });
 
 module.exports = router;
