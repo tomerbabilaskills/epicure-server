7e3cf36893246baca48047f875c7968e7da7db3f
Tomer Babila <tomerb@academy-skills.com>
2021-07-26 17:03:31 +0300

    Get all dishes and dish by id

--- api/dishRouter.js
+++ api/dishRouter.js
@@ -1,9 +1,27 @@
 const { Router } = require('express');
+const dishModel = require('../models/dishModel');
 
 const router = Router();
 
-router.get('/', (req, res) => {
-  res.send('hello dish');
+// [GET] - all dishes
+router.get('/', async (req, res) => {
+  try {
+    const dishes = await dishModel.find({});
+    res.json(dishes);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+// [GET] - dish by id
+router.get('/:id', async (req, res) => {
+  try {
+    const dishId = req.params.id;
+    const dish = await dishModel.findById(dishId);
+    res.json(dish);
+  } catch (error) {
+    res.status(500).send(error);
+  }
 });
 
 module.exports = router;
