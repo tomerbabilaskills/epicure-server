d9ca769b12fb495945b3417c9e9e643c3887d7d5
Tomer Babila <tomerb@academy-skills.com>
2021-07-26 16:59:33 +0300

    Get all chefs and chef by id

--- api/chefRouter.js
+++ api/chefRouter.js
@@ -1,9 +1,27 @@
 const { Router } = require('express');
+const chefModel = require('../models/chefModel');
 
 const router = Router();
 
-router.get('/', (req, res) => {
-  res.send('hello chef');
+// [GET] - all chefs
+router.get('/', async (req, res) => {
+  try {
+    const chefs = await chefModel.find({});
+    res.json(chefs);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+// [GET] - chef by id
+router.get('/:id', async (req, res) => {
+  try {
+    const chefId = req.params.id;
+    const chef = await chefModel.findById(chefId);
+    res.json(chef);
+  } catch (error) {
+    res.status(500).send(error);
+  }
 });
 
 module.exports = router;
