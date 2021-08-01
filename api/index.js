e741eddcee64038bcddf6ba32a5ff34260ee02e5
Tomer Babila <tomerb@academy-skills.com>
2021-08-01 09:25:53 +0300

    remove unnecessary routes

--- api/index.js
+++ api/index.js
@@ -2,13 +2,11 @@ const { Router } = require('express');
 const chefRouter = require('./chef/chef.routes');
 const restaurantRouter = require('./restaurant/restaurant.routes');
 const dishRouter = require('./dish/dish.routes');
-const seedRouter = require('./seedRouter');
 
 const router = Router();
 
 router.use('/chef', chefRouter);
 router.use('/restaurant', restaurantRouter);
 router.use('/dish', dishRouter);
-router.use('/seed', seedRouter);
 
 module.exports = router;
